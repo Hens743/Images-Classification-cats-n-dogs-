558fb4d43d51123516a351cd0c59f366d28fbe98
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-11-21 13:30:04 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -3,52 +3,52 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 from PIL import Image
-import gdown
+import tempfile
 import os
 
-@st.cache_resource
-def load_model():
+# Function to load a Keras model from an uploaded file
+def load_model(uploaded_file):
     try:
-        # Google Drive link for the model
-        google_drive_link = ''
-        # Path to save the downloaded model
-        model_path = 'model.h5'
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.keras') as tmp:
+            tmp.write(uploaded_file.getvalue())
+            tmp_path = tmp.name
 
-        # Check if the model is already downloaded
-        if not os.path.isfile(model_path):
-            # Download the model from Google Drive
-            gdown.download(google_drive_link, model_path, quiet=False)
+        model = keras.models.load_model(tmp_path)
 
-        # Load the model
-        model = keras.models.load_model(model_path)
+        # Optionally, remove the temporary file if desired
+        os.remove(tmp_path)
+
         return model
     except Exception as e:
         st.error(f"Error loading model: {e}")
         return None
 
-model = load_model()
+st.title('Image classification web app')
 
-class_names = ['Cat', 'Dog']  # Model's classes
+model_upload = st.file_uploader('Upload a model file', type=['h5', 'keras'])
 
-st.title('Image Classification Web App')
+if model_upload is not None:
+    model = load_model(model_upload)
+    if model:
+        st.success("Model loaded successfully!")
 
-uploaded_image = st.file_uploader('Upload an image of dog or cat', type=['jpg', 'png', 'jpeg'])
+        uploaded_image = st.file_uploader('Upload an image of a DOG or CAT', type=['jpg', 'png', 'jpeg'])
 
-if uploaded_image is not None:
-    st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
-    
-    try:
-        image = Image.open(uploaded_image)
-        image = image.resize((100, 100))  # Adjust size as needed
-        image = np.array(image)
-        image = image / 255.0  # Normalize
+        if uploaded_image is not None:
+            st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
 
-        if model:
-            predictions = model.predict(np.expand_dims(image, axis=0))
-            predicted_class_index = np.argmax(predictions)
-            predicted_class = class_names[predicted_class_index]
-            confidence_score = np.max(predictions) * 100  # Confidence score in percentage
+            try:
+                image = Image.open(uploaded_image)
+                image = image.resize((100, 100))  # Adjust size as needed
+                image = np.array(image)
+                image = image / 255.0  # Normalize
 
-            st.write(f'Prediction: {predicted_class} (Confidence: {confidence_score:.2f}%)')
-    except Exception as e:
-        st.error(f"Error processing image: {e}")
+                predictions = model.predict(np.expand_dims(image, axis=0))
+                class_names = ['CAT', 'DOG']  # Model's classes
+                predicted_class_index = np.argmax(predictions)
+                predicted_class = class_names[predicted_class_index]
+                confidence_score = np.max(predictions) * 100  # Confidence score in percentage
+
+                st.write(f'Prediction: {predicted_class} (Confidence: {confidence_score:.2f}%)')
+            except Exception as e:
+                st.error(f"Error processing image: {e}")
